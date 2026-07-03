62d9be20a45c5dfb5195e7dc2e40bab3a7199a58
karanX03 <karanpandey801@gmail.com>
2026-07-03 12:44:08 +0530

    Update archive.js

--- archive.js
+++ archive.js
@@ -1,20 +1,22 @@
-document.querySelectorAll(".card").forEach((card,index)=>{
-    card.style.opacity="0";
-    card.style.transform="translateY(40px)";
+document.querySelectorAll(".card").forEach((card, index) => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(40px)";
 
-    setTimeout(()=>{
-        card.style.transition="all .8s ease";
-        card.style.opacity="1";
-        card.style.transform="translateY(0)";
-    },300*(index+1));
+    setTimeout(() => {
+        card.style.transition = "all .8s ease";
+        card.style.opacity = "1";
+        card.style.transform = "translateY(0)";
+    }, 300 * (index + 1));
 });
+
 function toggleLetter(btn) {
-  const letter = btn.nextElementSibling;
+    const letter = btn.nextElementSibling;
 
-  letter.classList.toggle("show");
+    letter.classList.toggle("show");
 
-  if (letter.classList.contains("show")) {
-    btn.innerText = "Hide";
-  } else {
-    btn.innerText = "Read More";
-  }
+    if (letter.classList.contains("show")) {
+        btn.innerText = "Hide";
+    } else {
+        btn.innerText = "Read More";
+    }
+}
