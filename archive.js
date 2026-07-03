5d0e35dbbd853513345c8125457faab70f5b9ec7
karanX03 <karanpandey801@gmail.com>
2026-07-03 12:26:53 +0530

    Update archive.js

--- archive.js
+++ archive.js
@@ -8,3 +8,13 @@ document.querySelectorAll(".card").forEach((card,index)=>{
         card.style.transform="translateY(0)";
     },300*(index+1));
 });
+function toggleLetter(btn) {
+  const letter = btn.nextElementSibling;
+
+  letter.classList.toggle("show");
+
+  if (letter.classList.contains("show")) {
+    btn.innerText = "Hide";
+  } else {
+    btn.innerText = "Read More";
+  }
