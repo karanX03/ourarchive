143e42b06072d1c30cacb2a0fdd161fb315680b5
karanX03 <karanpandey801@gmail.com>
2026-07-03 09:38:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,27 +14,6 @@ function moveButton() {
 }
 
 yesBtn.addEventListener("click", () => {
-  document.body.innerHTML = `
-    <div style="
-      height:100vh;
-      display:flex;
-      justify-content:center;
-      align-items:center;
-      flex-direction:column;
-      background:linear-gradient(135deg,#ff5fa2,#ffd6e8);
-      color:white;
-      text-align:center;
-      font-family:Arial,sans-serif;
-      padding:20px;
-    ">
-      <h1 style="font-size:42px;">❤️ Yay! ❤️</h1>
-      <h2>You made Karan the happiest person alive.</h2>
-      <p style="font-size:20px;margin-top:20px;">
-        Thank you for choosing my heart. 🌹<br><br>
-        I promise to love you, respect you,<br>
-        and always stand beside you. ❤️
-      </p>
-      <h1 style="margin-top:30px;">Karan ❤️ Shristi</h1>
-    </div>
-  `;
+  
+    window.location.href = "archive.html";
 });
